660def2d795746ea92db6fb2b486380620c40296
Timur <bratokai@gmail.com>
2022-02-18 22:06:39 +0100

    registration_01

---
+++ create_acc.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF=8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name = "viewport" content="widht=device-widht, initial-scale=1.0" >
+
+    <title>create_acc</title>
+
+    <body>
+    <a href="index.html">Index</a>
+        <?php
+            $user = $_REQUEST['user'];
+            
+            $pwd = $_REQUEST['pwd'];
+            $hash = hash("sha256", $pwd);
+           
+            include("../../params//billing.php");
+
+            $conn = mysqli_connect($db_server,$db_user,$db_pwd,"billing");
+            $sql = "INSERT INTO users(Login, Pwdhash, Adress) VALUES($user,$pwd,$Adress_var)";
+            //echo $sql;
+
+            mysqli_query($conn, $sql);
+
+            
+            
+           /* $sql = "SELECT * FROM users WHERE login = '$user' AND Pwdhash='$hash'  " ;
+
+            $query = mysqli_query($conn,$sql);
+            $result = mysqli_fetch_all($query);
+            использование параметрического запроса защищает нас от sql injection */ 
+
+
+
+
+              /*  $sql = "SELECT * FROM users WHERE login=? AND Pwdhash=?" ;
+                $statement = mysqli_prepare($conn,$sql);
+                mysqli_stmt_bind_param($statement, "ss", $user, $hash);
+                mysqli_stmt_execute($statement);
+                $cursor = mysqli_stmt_get_result($statement);
+                $result= mysqli_fetch_all($cursor); */
+        
+
+            mysqli_close($conn);
+            //var_dump($result);
+            if (count($result) == 0)
+                echo ("Operation fault");
+
+            else {
+                echo "<h1> Welcome, $user! </h1>";
+                echo "Перенаправление...";
+               
+                $_SESSION["user"] = $user;
+                echo '<meta http-equiv="refresh" content="2; URL=index.html"> ';
+            }
+
+
+        ?>
+
+    </body>
+</html>
---
+++ registration.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF=8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name = "viewport" content="widht=device-widht, initial-scale=1.0" >
+
+    <title>Registration</title>
+</head>
+<body>
+    <h1>For creating account, please input name and password und your adress</h1>
+    <form method="POST" action="create_acc.php">
+    name<br />   
+    <input name="user"/><br />
+    password<br />   
+    <input type="password" name="pwd"/><br />
+     adress<br />
+    <input name="Adress_var"/><br />
+    <button>Done!</button>
+
+
+
+
+
+    </form>
+
+
+
+</body>
+
+</html>
